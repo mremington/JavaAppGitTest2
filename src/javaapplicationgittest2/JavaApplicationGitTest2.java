3b437027c42dab54e1d3224aa7dceeecdce76a83
mremington <michael.remington@sduhsd.net>
2019-02-26 09:14:21 -0800

    Add message

--- src/javaapplicationgittest2/JavaApplicationGitTest2.java
+++ src/javaapplicationgittest2/JavaApplicationGitTest2.java
@@ -17,6 +17,7 @@ public class JavaApplicationGitTest2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello Netbeans!");
+        System.out.println("Good Connection!");
         
     }
     
